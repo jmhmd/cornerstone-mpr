b20a2254ccf74889805757adab5f01715c3a7b61
jmhmd <jhostetter@gmail.com>
2018-10-27 14:37:03 -0400

    update images again

--- images/get-list.js
+++ images/get-list.js
@@ -6,11 +6,11 @@ fs.writeFileSync(
   JSON.stringify({
     'ax-lung': fs
       .readdirSync(__dirname + '/ax-lung')
-      .map(fn => `wadouri:/images/ax-lung/${fn}`)
+      .map(fn => `wadouri:images/ax-lung/${fn}`)
       .filter(fn => fn.substring(fn.length - 4) === '.dcm'),
     't1-brain': fs
       .readdirSync(__dirname + '/t1-brain')
-      .map(fn => `wadouri:/images/t1-brain/${fn}`)
+      .map(fn => `wadouri:images/t1-brain/${fn}`)
       .filter(fn => fn.substring(fn.length - 4) === '.dcm'),
   })
 );
